51e8b9df8463d7d4a7c6770196e63f038988efb2
tenmushi <tentoumushi0319@icloud.com>
2026-03-14 14:08:19 +0900

    question2

--- src/math.test.js
+++ src/math.test.js
@@ -5,12 +5,6 @@ test("sqrt", () => {
 });
 
 // ここに例外のテストコードを追加
-import { sqrt } from "./math.js";
-
-test("sqrt", () => {
-  expect(sqrt(9)).toBe(3);
-});
-
 test("negative number", () => {
   expect(() => sqrt(-1)).toThrow();
 });
